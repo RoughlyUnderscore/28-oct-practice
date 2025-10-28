f4c67410f200a21cb1e6642441e5b76f449fe628
Roughly_ <75266403+RoughlyUnderscore@users.noreply.github.com>
2025-10-28 23:41:14 +0500

    task 2

---
+++ src/2.py
@@ -0,0 +1,38 @@
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+  @abstractmethod
+  def _info(self) -> str:
+    pass
+
+  def __str__(self):
+    return self._info()
+
+
+class Car(Vehicle):
+  def _info(self):
+    return "I am a car! I can zoom across the highway blazingly fast."
+
+
+class Bus(Vehicle):
+  def _info(self):
+    return "I am a bus! I hold many passengers but sometimes it gets really claustrophobic here."
+
+
+class Train(Vehicle):
+  def _info(self):
+    return "I am a train and I am the most ecologically clean means of transport!"
+
+
+car = Car()
+bus = Bus()
+train = Train()
+
+print(car)
+print(bus)
+print(train)
+
+# or:
+print(car._info())
+print(bus._info())
+print(train._info())
